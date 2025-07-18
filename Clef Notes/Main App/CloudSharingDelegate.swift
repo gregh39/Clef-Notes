28674341ee1bcff07bdcfe2adee451c4b4c2cd95
Greg Holland <greg2257@gmail.com>
2025-07-18 17:35:35 -0500

    Removed all sharing stuff

--- Clef Notes/Main App/CloudSharingDelegate.swift
+++ Clef Notes/Main App/CloudSharingDelegate.swift
@@ -9,5 +9,5 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
-    
+        
 }
